ef33743a890d90f904389c820de516cda56a8943
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2026-02-17 17:59:03 +0330

    feat: implement image upload & refactor to express 5

---
+++ src/utils/upload.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import AppError from "../utils/appError.js";
+
+// 1. Save in memory as buffer
+const multerStorage = multer.memoryStorage();
+
+// 2. File filter to accept only images
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Only images are allowed", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+export const uploadProductImage = upload.array("images", 5);
